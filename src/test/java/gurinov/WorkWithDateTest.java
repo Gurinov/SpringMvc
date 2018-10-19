3194f73d94a4f1506ebb3a3d55aba221f5769f92
Gurinov <https://github.com/Gurinov/E.Gurinov.git>
2018-10-19 17:08:42 +0300

    Rewrite tests

--- src/test/java/gurinov/WorkWithDateTest.java
+++ src/test/java/gurinov/WorkWithDateTest.java
@@ -9,16 +9,60 @@ import static org.junit.Assert.*;
 public class WorkWithDateTest {
 
     @Test
-    public void isValidDate() {
+    public void isValidSeparatedSlash() {
         assertTrue(WorkWithDate.isValidDate("12/12/12"));
+    }
+
+    @Test
+    public void isValidZeroAheadDay() {
         assertTrue(WorkWithDate.isValidDate("02/02/12"));
+    }
+
+
+    @Test
+    public void isValidSeparatedDash() {
         assertTrue(WorkWithDate.isValidDate("12-12-2010"));
+    }
+
+
+    @Test
+    public void isValidSingleDigitDay() {
         assertTrue(WorkWithDate.isValidDate("2-12-10"));
+    }
+
+
+    @Test
+    public void isLeapYear() {
         assertTrue(WorkWithDate.isValidDate("29-2-2000"));
+    }
+
+
+    @Test
+    public void isNotLeapYear() {
         assertFalse(WorkWithDate.isValidDate("29-2-99"));
+    }
+
+
+    @Test
+    public void isNonexistentDate() {
         assertFalse(WorkWithDate.isValidDate("0-0-0"));
+    }
+
+
+    @Test
+    public void isInvalidMonth() {
         assertFalse(WorkWithDate.isValidDate("10-13-2000"));
+    }
+
+
+    @Test
+    public void isInvalidDay() {
         assertFalse(WorkWithDate.isValidDate("32-10-2000"));
+    }
+
+
+    @Test
+    public void isInvalidYear() {
         assertFalse(WorkWithDate.isValidDate("10-10-9999"));
     }
 
